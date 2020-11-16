575e6ef7e6c0aca3d87501f6aada46cb0af41e74
MrDML <dml1630@163.com>
2020-11-16 10:45:47 +0800

    update sdk

--- LinKingSDK/Products/LinKingSDK.framework/Headers/LKUserControllerView.h
+++ LinKingSDK/Products/LinKingSDK.framework/Headers/LKUserControllerView.h
@@ -22,7 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (weak, nonatomic) IBOutlet UIImageView *imageView_message;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView_order;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView_binding;
-@property (weak, nonatomic) IBOutlet UIButton *button_fixIcon;
+@property (weak, nonatomic) IBOutlet UIButton *button_binding_account;
 @property (weak, nonatomic) IBOutlet UIButton *button_change_account;
 @property(nonatomic, copy)void(^closeViewAction)(UIButton *sender);
 @property(nonatomic, copy)void(^fixIconAction)(UIButton *sender);
@@ -30,6 +30,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nonatomic, copy)void(^changeAccountAction)(UIButton *sender);
 @property (weak, nonatomic) IBOutlet UIView *view_msg_circle;
 
+@property (weak, nonatomic) IBOutlet UILabel *label_real_name;
+
+@property(nonatomic, copy)void(^bindingAccountAction)(UIButton *sender);
 + (instancetype)instanceUserControllerView;
 @end
 
