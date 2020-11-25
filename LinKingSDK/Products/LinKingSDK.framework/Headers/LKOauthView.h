b31ba3d383d8cc5d26e0f5063d1b67b7c0ba25c7
MrDML <dml1630@163.com>
2020-11-25 11:33:19 +0800

    update

--- LinKingSDK/Products/LinKingSDK.framework/Headers/LKOauthView.h
+++ LinKingSDK/Products/LinKingSDK.framework/Headers/LKOauthView.h
@@ -26,7 +26,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nonatomic, copy)void(^thirdLoginAction)(UIButton *sender);
 @property(nonatomic, copy)void(^closeAlterViewCallBack)(void);
 @property(nonatomic, copy)void(^useAgreementCallBack)(BOOL isAgreement,UIButton *sender);
-
+@property(nonatomic, copy)void(^textFieldBenginEditCallBack)(CGFloat startRealHeight);
+@property (weak, nonatomic) IBOutlet UIView *view_switch;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *layoutHeight;
 @property (weak, nonatomic) IBOutlet UIImageView *imageViewHeader;
 @property (weak, nonatomic) IBOutlet UITextField *textfield_iphone;
@@ -35,36 +36,64 @@ NS_ASSUME_NONNULL_BEGIN
 @property (weak, nonatomic) IBOutlet UITextField *textfield_code;
 @property (weak, nonatomic) IBOutlet UIButton *button_get_code;
 @property (weak, nonatomic) IBOutlet UITextField *textfield_pwd;
+
+
+@property (weak, nonatomic) IBOutlet UIView *view_checkCode;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_checkCode_height;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_checkCode_top;
+@property (weak, nonatomic) IBOutlet UIView *view_checkout_line_one;
+@property (weak, nonatomic) IBOutlet UIView *view_checkout_line_two;
+
 @property (weak, nonatomic) IBOutlet UIView *view_pwd;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_pwd_height;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_insert_top;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_pwd_top;
 @property (weak, nonatomic) IBOutlet UILabel *label_pwd;
-@property (weak, nonatomic) IBOutlet UIView *view_line;
-@property (weak, nonatomic) IBOutlet UIView *view_line_two;
+@property (weak, nonatomic) IBOutlet UIView *view_pwd_line_one;
+@property (weak, nonatomic) IBOutlet UIView *view_pwd_line_two;
+
+
+
+@property (weak, nonatomic) IBOutlet UILabel *label_otherLogin;
+@property (weak, nonatomic) IBOutlet UIView *label_otherLogin_line_one;
+@property (weak, nonatomic) IBOutlet UIView *lable_otherLogin_line_two;
+
 
 @property (weak, nonatomic) IBOutlet UIButton *button_close;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView_close;
-@property (weak, nonatomic) IBOutlet UIButton *button_close_two;
-@property (weak, nonatomic) IBOutlet UIImageView *imageView_close_two;
-
 @property (weak, nonatomic) IBOutlet UIButton *button_protocol;
 @property (weak, nonatomic) IBOutlet UIView *view_apple;
 @property (weak, nonatomic) IBOutlet UIView *view_weixin;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_right_space;
 
+@property (weak, nonatomic) IBOutlet UIView *view_third_oauth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_third_oauth_height;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_third_oauth_height_top;
 @property (weak, nonatomic) IBOutlet UIButton *button_check;
 @property (weak, nonatomic) IBOutlet UIButton *button_login;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_login_height;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layout_login_top;
+
+
 @property (weak, nonatomic) IBOutlet UIButton *button_quick;
 @property (weak, nonatomic) IBOutlet UIButton *button_weixin;
+
 @property (nonatomic, assign) NSInteger switchIndex;
 @property (assign, nonatomic) BOOL isNewUser;
 @property (assign, nonatomic) SignInStyle signInStyle;
+@property (nonatomic, assign) CGFloat deductionHeight; // 扣减高度
+@property (nonatomic, assign) CGFloat totalHeight; //
+@property (nonatomic, assign) CGFloat startRealHeight;
 + (instancetype)instanceOauthView;
 - (void)showViewPassword;
 - (void)hiddenViewPassword;
 - (void)setLKSuperView:(UIView *)superView;
 - (void)updatePasswordView;
 - (void)updateCodeView;
+
+- (void)showButtonLogin;
+- (void)hiddenButtonLogin;
+
+- (void)showViewChechView;
+- (void)hiddenViewChechView;
 @end
 
 NS_ASSUME_NONNULL_END
